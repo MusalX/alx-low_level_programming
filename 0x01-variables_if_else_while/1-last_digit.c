566d88af0afd10323b1d6113644940d4c7a44a6e
MusalX <salhi.mustafa@gmail.com>
2023-06-21 15:13:19 -0700

    last digit using number % 10 which is le reste de la division

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * print last digit of a random n and say his range
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int ld = n % 10;
+
+	if (ld > 6)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	}
+	else if (ld == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, ld);
+	}
+	else
+	{
+		printf("Last digiit of %d is %d and is less than 6 and not 0\n", n, ld);
+	}
+	return (0);
+}
