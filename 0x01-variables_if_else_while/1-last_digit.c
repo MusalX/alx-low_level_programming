e138e42feb4d41ed93e2d02daebdff96f6b7e78c
MusalX <salhi.mustafa@gmail.com>
2023-06-21 15:26:52 -0700

    last digit using number % 10 which is le reste de la division

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,11 +11,12 @@
 int main(void)
 {
 	int n;
-	int ld = n % 10;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	ld = n % 10;
 	if (ld > 6)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
