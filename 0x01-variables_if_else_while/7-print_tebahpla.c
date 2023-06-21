5256a98b1cdeb03a23ca4d58624f61e3519d0b49
MusalX <salhi.mustafa@gmail.com>
2023-06-21 16:39:38 -0700

    in reverse, using while but with i--

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * alphabet in reverse
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i = 122;
+
+while (i > 96)
+{
+	putchar(i);
+	i--;
+}
+putchar('\n');
+	return (0);
+}
