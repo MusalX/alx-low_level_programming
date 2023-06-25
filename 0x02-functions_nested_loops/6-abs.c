30f912569cab6987d08ae3019ced5e0914651915
MusalX <salhi.mustafa@gmail.com>
2023-06-25 16:27:35 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - absolute value
+ * @i: to be checked
+ * Return: Absolute value
+ */
+int _abs(int i)
+{
+if (i >= 0)
+{
+	return (i);
+}
+else
+{
+	return (i * -1);
+}
+}
