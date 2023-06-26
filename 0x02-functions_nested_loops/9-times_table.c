814ddc5db145d2902c2d9fcd8952445dc3834675
MusalX <salhi.mustafa@gmail.com>
2023-06-25 18:01:29 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,16 +5,24 @@
  */
 void times_table(void)
 {
-int i, j, p;
+int i, j, p, d, u;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 		p = i * j;
-		_putchar(p + 48);
+		d = p / 10;
+		u = p % 10;
+		_putchar(d + 48);
+		_putchar(u + 48);
 		_putchar(',');
 		}
-		_putchar('\n');
+		if (j == 9)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('\n');
+		}
 	}
-}
+	}
