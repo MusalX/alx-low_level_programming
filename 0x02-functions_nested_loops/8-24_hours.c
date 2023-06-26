3cac4185f2afd0a53a674ec882f9a32f1c56114e
MusalX <salhi.mustafa@gmail.com>
2023-06-25 17:26:06 -0700

    print minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *jack_bauer -  print minutes
+ * Return:  nada
+ */
+void jack_bauer(void)
+{
+int a, b, c, d;
+for (a = 0; a < 3; a++)
+{
+	for (b = 0; b < 10; b++)
+	{
+		for (c = 0; c < 7; c++)
+		{
+			for (d = 0; d < 10; d++)
+			{
+				if (a == 2 && b > 3)
+					break;
+				_putchar(a + 48);
+				_putchar(b + 48);
+				_putchar(58);
+				_putchar(c + 48);
+				_putchar(d + 48);
+				_puthcar('\n');
+			}
+		}
+	}
+}
+}
+
